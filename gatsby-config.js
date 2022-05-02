b41293c10821807d7fa0f49877bb9c30a540e393
Gage Krumbach <gkrumbach@gmail.com>
2022-05-02 10:52:47 -0500

    added cron job and fixed bug

--- gatsby-config.js
+++ gatsby-config.js
@@ -89,9 +89,8 @@ module.exports = {
         `,
         feeds: [
           {
-            serialize: ({ query: { site, allMarkdownRemark } }) => {
-              console.log(allMarkdownRemark)
-              return allMarkdownRemark.nodes.map(node => {
+            serialize: ({ query: { site, allMdx } }) => {
+              return allMdx.nodes.map(node => {
                 return Object.assign({}, node.frontmatter, {
                   description: node.excerpt,
                   date: node.frontmatter.date,
